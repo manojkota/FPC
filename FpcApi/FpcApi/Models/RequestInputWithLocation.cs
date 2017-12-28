81ca51c0294d04f199ee1d5af9318db90810157d
leithboyd <leith.mckendry@outlook.com>
2017-12-28 15:53:54 +1100

    Added Endpoint

---
+++ FpcApi/FpcApi/Models/RequestInputWithLocation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using FpcApi.Common;
+
+namespace FpcApi.Models
+{
+    public class RequestInputWithLocation
+    {
+        public RequestInput Origin { get; set; }
+
+        public IList<Coordinate> Waypoints { get; set; }
+
+        public long LocationId { get; set; }
+    }
+}
